67263e5b6bea79b3cb0f2fd5301821e359c9f0ac
avnish98 <avnish07pal@gmail.com>
2020-11-02 16:47:55 +0530

    added column name option to standardizer and normalizer

--- modules/processor.py
+++ modules/processor.py
@@ -7,26 +7,34 @@ class Processor():
     def __init__(self, vardict=VarDict()):
         self.vardict = vardict
     
-    def standardizer(self, var_name, new_var=True, new_var_name="_"):
+    def standardizer(self, var_name, col_name, new_var=True, new_var_name="_"):
         if((new_var==True) and (new_var_name=="_")):
             print("A null variable is created with name '_'.\n Please move it to some other variable name.\n Using method move_empty()")    
 
         scale = StandardScaler()
         
         var = find_in_vardict(self.vardict, var_name)
-        var_data = var["data"].select_dtypes(include=['float64', 'int64'])
+        
+        if col_name:
+            var_data = var["data"][col_name]
+        else:
+            var_data = var["data"].select_dtypes(include=['float64', 'int64'])
+
         scaled_data = scale.fit_transform(var_data.values)
 
         self.vardict.add(scaled_data, new_var_name)
 
-    def normalizer(self, var_name, new_var=True, new_var_name="_"):
+    def normalizer(self, var_name, col_name, new_var=True, new_var_name="_"):
         if((new_var==True) and (new_var_name=="_")):
             print("A null variable is created with name '_'.\n Please move it to some other variable name.\n Using method move_empty()")    
        
         scale = MinMaxScaler()
         
         var = find_in_vardict(self.vardict, var_name)
-        var_data = var["data"].select_dtypes(include=['float64', 'int64'])
+        if col_name:
+            var_data = var["data"][col_name]
+        else:
+            var_data = var["data"].select_dtypes(include=['float64', 'int64'])
         scaled_data = scale.fit_transform(var_data.values)
 
         self.vardict.add(scaled_data, new_var_name)
