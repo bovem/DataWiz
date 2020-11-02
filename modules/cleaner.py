2b4619062b6ef375139d8a8ea0fd63d6666eb196
avnish98 <avnish07pal@gmail.com>
2020-11-02 14:20:43 +0530

    Almost finished with cleaner script

---
+++ modules/cleaner.py
@@ -0,0 +1,77 @@
+from utils import find_in_vardict, check_for_df
+from var_dict import VarDict
+
+class Cleaner():
+    def __init__(self, vardict=VarDict()):
+        self.vardict =  vardict
+
+    def remove_null(self, var_name, new_var=False, new_var_name=""):
+        if check_for_df(self.vardict, var_name):
+            var = find_in_vardict(self.vardict, var_name)
+            print("Size before dropping nulls: {}".format(var["data"].shape))
+            if new_var:
+                self.vardict.add(var["data"].dropna(), new_var_name)
+                print("Created Variable name: {}".format(new_var_name))
+            else:
+                var["data"].dropna(inplace=True)
+            print()
+            print("Size after dropping nulls: {}".format(var["data"].shape))
+    
+    def mapper(self, var_name, col_name="", maping={}, new_var=False, new_var_name=""):
+        if check_for_df(self.vardict, var_name):
+            var = find_in_vardict(self.vardict, var_name)
+            if new_var:
+                self.vardict.add(var["data"], new_var_name)
+                var2 = find_in_vardict(self.vardict, new_var_name)
+                print("Created Variable name: {}".format(new_var_name))
+                var2["data"]["mapped_{}".format(col_name)] = var2["data"]["{}".format(col_name)].map(maping)
+            else:
+                var["data"]["mapped_{}".format(col_name)] = var["data"]["{}".format(col_name)].map(maping)
+    
+    def fill_mean(self, var_name, new_var=False, new_var_name=""):
+        if check_for_df(self.vardict, var_name):
+            var = find_in_vardict(self.vardict, var_name)
+            #print("Size before dropping nulls: {}".format(var["data"].shape))
+            if new_var:
+                self.vardict.add(var["data"].fillna(var["data"].mean()), new_var_name)
+                print("Created Variable name: {}".format(new_var_name))
+            else:
+                var["data"].fillna(var["data"].fillna(var_data.mean()), inplace=True)
+            #print()
+            #print("Size after dropping nulls: {}".format(var["data"].shape))
+
+    def fill_median(self, var_name, new_var=False, new_var_name=""):
+        if check_for_df(self.vardict, var_name):
+            var = find_in_vardict(self.vardict, var_name)
+            #print("Size before dropping nulls: {}".format(var["data"].shape))
+            if new_var:
+                self.vardict.add(var["data"].fillna(var["data"].median()), new_var_name)
+                print("Created Variable name: {}".format(new_var_name))
+            else:
+                var["data"].fillna(var["data"].fillna(var_data.median()), inplace=True)
+            #print()
+            #print("Size after dropping nulls: {}".format(var["data"].shape))
+
+    def fill_forward(self, var_name, new_var=False, new_var_name=""):
+        if check_for_df(self.vardict, var_name):
+            var = find_in_vardict(self.vardict, var_name)
+            #print("Size before dropping nulls: {}".format(var["data"].shape))
+            if new_var:
+                self.vardict.add(var["data"].fillna("ffill"), new_var_name)
+                print("Created Variable name: {}".format(new_var_name))
+            else:
+                var["data"].fillna(var["data"].fillna("ffill"), inplace=True)
+            #print()
+            #print("Size after dropping nulls: {}".format(var["data"].shape))
+    
+    def fill_backward(self, var_name, new_var=False, new_var_name=""):
+        if check_for_df(self.vardict, var_name):
+            var = find_in_vardict(self.vardict, var_name)
+            #print("Size before dropping nulls: {}".format(var["data"].shape))
+            if new_var:
+                self.vardict.add(var["data"].fillna("bfill"), new_var_name)
+                print("Created Variable name: {}".format(new_var_name))
+            else:
+                var["data"].fillna(var["data"].fillna("bfill"), inplace=True)
+            #print()
+            #print("Size after dropping nulls: {}".format(var["data"].shape))
--- modules/test.py
+++ modules/test.py
@@ -1,4 +1,10 @@
 from loader import Loader
+from cleaner import Cleaner
 l = Loader()
 df = l.load("limestone_2020.csv", "limestone")
-print(l.vardict.var_dict[0]["data"])
+c = Cleaner(l.vardict)
+#c.remove_null("limestone")
+#c.mapper("limestone", "Year", {2020:"This_year"}, new_var=True, new_var_name="this_var")
+#c.fill_backward("limestone", new_var=True, new_var_name="some_var")
+print(c.vardict.show_data("some_var").info())
+
